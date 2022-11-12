d5fccf2186f681623e90c8d773995b4d7a8720aa
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-11-12 00:44:38 -0500

    Secured implementation

--- index.spec.js
+++ index.spec.js
@@ -7,26 +7,82 @@ describe('punybind', () => {
   })
 
   describe('binding function', () => {
-    const dom = new JSDOM(`<!DOCTYPE html>
-<html>
-  <head>
-    <title>Title : {{ title }}</title>
-  </head>
-  <body>
-  </body>
-</html>`)
-
     it('returns a function', () => {
+      const dom = new JSDOM('<head><title>Title : {{ title }}</title></head>')
       const update = punybind(dom.window.document.head)
       expect(typeof update).toBe('function')
     })
 
-    it('updates the HTML when calling the returned function', async () => {
-      const update = punybind(dom.window.document.head)
-      await update({
-        title: 'Hello World !'
+    describe('node binding', () => {
+      const invalidSyntaxes = [
+        'Title',
+        'Title : {{ title',
+        'Title : {{ title }',
+        'Title : { { title }}',
+        'Title : {{ title } }',
+        'Title : {{}}title}}'
+      ]
+
+      invalidSyntaxes.forEach(invalidSyntax => {
+        it(`ignores invalid syntax: ${invalidSyntax}`, async () => {
+          const dom = new JSDOM(`<head><title>${invalidSyntax}</title></head>`)
+          const update = punybind(dom.window.document.head)
+          expect(update.bindingsCount).toBe(0)
+          await update({
+            title: 'Hello World !'
+          })
+          expect(dom.window.document.title).toBe(invalidSyntax)
+        })
       })
-      expect(dom.window.document.title).toBe('Title : Hello World !')
+
+      it('mixes static and dynamic content', async () => {
+        const dom = new JSDOM('<head><title>Title is {{ title }}.</title></head>')
+        const update = punybind(dom.window.document.head)
+        await update({
+          title: 'Hello World !'
+        })
+        expect(dom.window.document.title).toBe('Title is Hello World !.')
+      })
+
+      it('empties the content if an error occurs', async () => {
+        const dom = new JSDOM('<head><title>Title : {{ error }}</title></head>')
+        const update = punybind(dom.window.document.head)
+        await update({
+          get error () {
+            throw new Error()
+          }
+        })
+        expect(dom.window.document.title).toBe('')
+      })
+
+      it('changes the node only when the value changes', async () => {
+        const dom = new JSDOM('<head><title id="title">Title : {{ title }}</title></head>')
+        let changes = 0
+        const observer = new dom.window.MutationObserver(() => ++changes)
+        observer.observe(dom.window.document.head, {
+          attributes: true,
+          childList: true,
+          subtree: true
+        })
+        const update = punybind(dom.window.document.head)
+        await update({
+          title: 'Hello World !'
+        })
+        expect(changes).toBe(1)
+        await update({
+          title: 'Hello World !'
+        })
+        expect(changes).toBe(1)
+      })
+
+      it('securely injects the content', async () => {
+        const dom = new JSDOM('<body>{{ inject }}</body>')
+        const update = punybind(dom.window.document.body)
+        update({
+          inject: '<script>alert(0)</alert>'
+        })
+        expect(dom.window.document.body.innerHTML).toBe('&lt;script&gt;alert(0)&lt;/alert&gt;')
+      })
     })
   })
 })
