3658b61d91d9544946335a944339579cf2992c46
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-11-15 07:31:16 -0500

    Testing reactive behavior

--- index.spec.js
+++ index.spec.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const { punybind } = require('./index.js')
 const { JSDOM } = require('jsdom')
 
@@ -50,8 +52,9 @@ describe('punybind', () => {
       })
 
       it('is read-only', () => {
-        update.bindingsCount = 2
-        expect(update.bindingsCount).toBe(1)
+        expect(() => {
+          update.bindingsCount = 2
+        }).toThrowError()
       })
 
       it('cannot be reconfigured', () => {
@@ -426,6 +429,7 @@ describe('punybind', () => {
   describe('reactive model', () => {
     let dom
     let update
+    let model
 
     beforeEach(async () => {
       dom = new JSDOM(`<body>
@@ -433,14 +437,21 @@ describe('punybind', () => {
   <ul>
     <li
       {{for}}="item of items"
-      class="todo {{ item.done ? 'done' : '' }}"
+      class="todo{{ item.done ? ' done' : '' }}"
     >{{ item.text }}</li>
   </ul>
 </body>`)
       update = await punybind(dom.window.document.body, {
         title: 'Hello World !',
-        items: []
+        items: [{
+          done: false,
+          text: 'Forget about heavy frameworks'
+        }, {
+          done: true,
+          text: 'Adopt punybind'
+        }]
       })
+      model = update.model
     })
 
     describe('model property', () => {
@@ -450,9 +461,9 @@ describe('punybind', () => {
       })
 
       it('is read-only', () => {
-        const { model } = update
-        update.model = 0
-        expect(update.model).toBe(model)
+        expect(() => {
+          update.model = 0
+        }).toThrowError()
       })
 
       it('cannot be reconfigured', () => {
@@ -464,14 +475,172 @@ describe('punybind', () => {
       })
     })
 
-    it('triggers an initial update', () => {
-      expect(dom2json(dom.window.document.body)).toMatchObject({
-        body: [
-          { h1: ['Hello World !'] },
-          { ul: [
-            { template: expect.anything() }
-          ]}
-        ]
+    describe('done property', () => {
+      it('exposes a done property', () => {
+        expect(update.done).not.toBeUndefined()
+        expect(typeof update.done).toBe('function')
+      })
+
+      it('is read-only', () => {
+        expect(() => {
+          update.done = 0
+        }).toThrowError()
+      })
+
+      it('cannot be reconfigured', () => {
+        expect(() => {
+          Object.defineProperty(update, 'done', {
+            get () { return 0 }
+          }).toThrowError()
+        })
+      })
+    })
+
+    describe('reactive behavior', () => {
+      let changes
+
+      beforeEach(() => {
+        changes = 0
+        const observer = new dom.window.MutationObserver(() => ++changes)
+        observer.observe(dom.window.document.body, {
+          attributes: true,
+          childList: true,
+          subtree: true
+        })
+      })
+
+      it('triggers an initial update', () => {
+        expect(dom2json(dom.window.document.body)).toMatchObject({
+          body: [
+            { h1: ['Hello World !'] },
+            {
+              ul: [
+                { li: [{ '@class': 'todo' }, 'Forget about heavy frameworks'] },
+                { li: [{ '@class': 'todo done' }, 'Adopt punybind'] },
+                { template: expect.anything() }
+              ]
+            }
+          ]
+        })
+      })
+
+      it('updates when changing a property', async () => {
+        expect(changes).toBe(0)
+        model.title = 'Test'
+        await update.done()
+        expect(changes).toBe(1)
+        expect(dom2json(dom.window.document.body)).toMatchObject({
+          body: [
+            { h1: ['Test'] },
+            {
+              ul: [
+                { li: [{ '@class': 'todo' }, 'Forget about heavy frameworks'] },
+                { li: [{ '@class': 'todo done' }, 'Adopt punybind'] },
+                { template: expect.anything() }
+              ]
+            }
+          ]
+        })
+      })
+
+      it('debounces property changes', async () => {
+        expect(changes).toBe(0)
+        model.title = 'Test'
+        model.title = 'Test 2'
+        await update.done()
+        expect(changes).toBe(1)
+        expect(dom2json(dom.window.document.body)).toMatchObject({
+          body: [
+            { h1: ['Test 2'] },
+            {
+              ul: [
+                { li: [{ '@class': 'todo' }, 'Forget about heavy frameworks'] },
+                { li: [{ '@class': 'todo done' }, 'Adopt punybind'] },
+                { template: expect.anything() }
+              ]
+            }
+          ]
+        })
+      })
+
+      it('does not refresh if property value does not change', async () => {
+        expect(changes).toBe(0)
+        model.title = 'Hello World !'
+        await update.done()
+        expect(changes).toBe(0)
+        expect(dom2json(dom.window.document.body)).toMatchObject({
+          body: [
+            { h1: ['Hello World !'] },
+            {
+              ul: [
+                { li: [{ '@class': 'todo' }, 'Forget about heavy frameworks'] },
+                { li: [{ '@class': 'todo done' }, 'Adopt punybind'] },
+                { template: expect.anything() }
+              ]
+            }
+          ]
+        })
+      })
+
+      describe('array', () => {
+        it('detects item change', async () => {
+          model.items[0].done = true
+          await update.done()
+          expect(dom2json(dom.window.document.body)).toMatchObject({
+            body: [
+              { h1: ['Hello World !'] },
+              {
+                ul: [
+                  { li: [{ '@class': 'todo done' }, 'Forget about heavy frameworks'] },
+                  { li: [{ '@class': 'todo done' }, 'Adopt punybind'] },
+                  { template: expect.anything() }
+                ]
+              }
+            ]
+          })
+        })
+
+        it('detects list change (adding)', async () => {
+          model.items[2] = {
+            done: true,
+            text: 'It works !'
+          }
+          await update.done()
+          expect(dom2json(dom.window.document.body)).toMatchObject({
+            body: [
+              { h1: ['Hello World !'] },
+              {
+                ul: [
+                  { li: [{ '@class': 'todo' }, 'Forget about heavy frameworks'] },
+                  { li: [{ '@class': 'todo done' }, 'Adopt punybind'] },
+                  { li: [{ '@class': 'todo done' }, 'It works !'] },
+                  { template: expect.anything() }
+                ]
+              }
+            ]
+          })
+        })
+
+        it('detects list change (pushing)', async () => {
+          model.items.push({
+            done: true,
+            text: 'It works !'
+          })
+          await update.done()
+          expect(dom2json(dom.window.document.body)).toMatchObject({
+            body: [
+              { h1: ['Hello World !'] },
+              {
+                ul: [
+                  { li: [{ '@class': 'todo' }, 'Forget about heavy frameworks'] },
+                  { li: [{ '@class': 'todo done' }, 'Adopt punybind'] },
+                  { li: [{ '@class': 'todo done' }, 'It works !'] },
+                  { template: expect.anything() }
+                ]
+              }
+            ]
+          })
+        })
       })
     })
   })
