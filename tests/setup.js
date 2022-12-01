dc9744fce274721e7f60790595a2f4b70507a9a8
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-12-01 10:42:44 -0500

    Use safeWait

--- tests/setup.js
+++ tests/setup.js
@@ -5,16 +5,20 @@ global.JSDOM = JSDOM
 const { dom2json } = require('./dom2json.js')
 global.dom2json = dom2json
 
-beforeAll(async () => {
-  // sanity check
-  expect(typeof punybind).toBe('function')
-  const dom = new JSDOM('<head><title>Title : {{ title }}</title></head>')
+global.safeWait = async promise => {
   let timeoutId
   await Promise.race([
-    punybind(dom.window.document.head),
+    promise,
     new Promise((resolve, reject) => {
       timeoutId = setTimeout(() => reject(new Error('Timeout')), 1000)
     })
   ])
   clearTimeout(timeoutId)
+}
+
+beforeAll(async () => {
+  // sanity check
+  expect(typeof punybind).toBe('function')
+  const dom = new JSDOM('<head><title>Title : {{ title }}</title></head>')
+  await safeWait(punybind(dom.window.document.head))
 })
