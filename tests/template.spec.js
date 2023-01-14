84152d0cf24c2dcf971cb6d9c23797be15acdd4c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-01-14 00:07:52 -0500

    Explain why no code excludes TEMPLATE tags

--- tests/template.spec.js
+++ tests/template.spec.js
@@ -3,6 +3,7 @@
 describe('template tags', () => {
   it('does not alter existing templates', async () => {
     const dom = new JSDOM('<body><template>{{ binding }}</template></body>')
+    // template content can be enumerated with DOM childNodes property, hence
     const update = await punybind(dom.window.document.body)
     expect(update.bindingsCount === 0)
   })
